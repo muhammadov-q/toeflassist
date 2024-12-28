82e152acb49325fe92fce052f41535a9e1e4e5e7
Qobiljon Muhammadov <muhammadov_q@auca.kg>
2024-12-28 23:00:37 +0500

    Update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enables static export
+  trailingSlash: true, // Ensures URLs have trailing slashes
+  assetPrefix: './', // Use relative paths for assets
 };
 
 export default nextConfig;
