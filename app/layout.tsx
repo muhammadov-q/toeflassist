7c74de2e71dd9d1aa07359db10000cfaff276226
Qobiljon Muhammadov <muhammadov_q@auca.kg>
2025-01-07 19:04:34 +0600

    Update href

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,10 +32,10 @@ export default function RootLayout({
     <html lang="ru">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://muhammadov-q.github.io/toeflassist/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://muhammadov-q.github.io/toeflassist/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://muhammadov-q.github.io/toeflassist/favicon-16x16.png" />
+        <link rel="manifest" href="https://muhammadov-q.github.io/toeflassist/site.webmanifest" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
